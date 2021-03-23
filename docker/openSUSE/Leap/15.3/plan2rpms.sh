ff5a7db9c7805466fdc61716ec37bbd10336cddf
Mark Stopka <mark.stopka@perlur.cloud>
2021-03-23 04:09:13 +0100

    Update openSUSE Leap 15.3 image

---
+++ docker/openSUSE/Leap/15.3/plan2rpms.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DEPS_COUNT=$(jq '."install-plan" | length' < cardano-node/dist-newstyle/cache/plan.json)
+
+for foo in `seq 0 $(($DEPS_COUNT-1))`; do
+  PKG=`jq '."install-plan"['$foo']' < cardano-node/dist-newstyle/cache/plan.json`
+  PKG_STYLE=`echo $PKG | jq '.style'`
+  if [ "$PKG_STYLE" = '"global"' ]; then
+    echo $PKG | jq '."pkg-name"' | sed -e 's/^"//' -e 's/"$//' >> hackage-dependencies.txt
+  elif [ "$PKG_STYLE" = '"local"' ]; then
+    echo $PKG | jq '."pkg-name"' | sed -e 's/^"//' -e 's/"$//' >> local-dependencies.txt
+  else
+    true
+  fi
+done
+
+sort hackage-dependencies.txt | uniq > hackage-dependencies.txt.tmp
+mv hackage-dependencies.txt.tmp hackage-dependencies.txt
+
+while read -r line; do
+  if ZYPP_INFO=`zypper search -x ghc-$line-devel`; then
+    echo ghc-$line-devel >> installable-hackage-dependencies.txt
+  elif ZYPP_INFO=`zypper search -x $line`; then
+    echo $line >> installable-hackage-dependencies.txt
+  else
+    echo $line >> missing-hackage-dependencies.txt
+  fi
+done < hackage-dependencies.txt
