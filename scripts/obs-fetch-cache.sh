480450a6b460b608fd13305508b152060160a653
Mark Stopka <mark.stopka@perlur.cloud>
2021-03-24 00:41:34 +0100

    Add nightly cronjob GitHub Action (#19)
    * Add GitHub Actions nightly cronjob workflow
    * Enable OBS fetch
      - Add OBS fetch script
          - Add step-retry for obs-fetch
          - Add dirty OBS project checkout clean-up
      - Add OBS project configuration files
      - Add OSC configuration
      - Add python3-m2crypto package to enable OBS TLS
     - Add .gitignore

---
+++ scripts/obs-fetch-cache.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+if ! which osc >/dev/null 2>&1; then
+  echo "osc is not installed!"
+  exit 2
+fi
+
+if [ ! -f ~/.oscrc ]; then
+  echo '[general]' >> ~/.oscrc
+  echo 'apiurl = https://api.opensuse.org' >> ~/.oscrc
+  echo '' >> ~/.oscrc
+  echo '[https://api.opensuse.org/]' >> ~/.oscrc
+  echo 'realname = PERLUR OBS Automation' >> ~/.oscrc
+  echo 'email = perlur.cloud@gmail.com' >> ~/.oscrc
+  echo 'user=perlur-obs-automation' >> ~/.oscrc
+  echo 'pass='${PERLUR_OBS_AUTOMATION_PASS} >> ~/.oscrc
+fi
+
+OBS_PRJ_STABLE=$(jq -r '.stable' < obs/config/projects.json)
+OBS_PRJ_NIGHTLY=$(jq -r '.nightly' < obs/config/projects.json)
+
+function clean_dirty_project() {
+  for dir in $(ls); do
+    if [ ! -f ${dir}/*.spec ]; then
+      rm -rf ${dir}
+    fi
+  done
+}
+
+pushd obs/cache/
+
+if [ -d ${OBS_PRJ_STABLE} ]; then
+  pushd ${OBS_PRJ_STABLE}
+  clean_dirty_project
+  osc up
+  popd
+else
+  osc co ${OBS_PRJ_STABLE}
+fi
+
+if [ -d ${OBS_PRJ_NIGHTLY} ]; then
+  pushd ${OBS_PRJ_NIGHTLY}
+  clean_dirty_project
+  osc up
+  popd
+else
+  osc co ${OBS_PRJ_NIGHTLY}
+fi 
