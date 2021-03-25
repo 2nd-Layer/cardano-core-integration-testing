a660f43ef15f53cc0a711a2d47a7b32814478f0e
Mark Stopka <mark.stopka@perlur.cloud>
2021-03-25 17:26:34 +0100

    Make curl quiet

--- scripts/docker-hub-check.sh
+++ scripts/docker-hub-check.sh
@@ -8,9 +8,11 @@ DOCKER_APPLICATION=${DOCKER_APPLICATION}
 DOCKER_IMAGE_NAME="${DOCKER_BASE_IMAGE}-${DOCKER_APPLICATION}"
 echo "Fetching image version of 2ndlayer/${DOCKER_IMAGE_NAME} from Docker Hub"
 
-DOCKER_HUB_IMAGE_VER=$(curl -L --fail \
+DOCKER_HUB_IMAGE_VER=$(curl -L --fail -q \
     "https://hub.docker.com/v2/repositories/2ndlayer/${DOCKER_IMAGE_NAME}/tags/?page_size=1000" | \
      jq '.results | .[] | .name' -r | \
      sed 's/latest//' | \
      sort --version-sort | \
     tail -n 1)
+
+echo "Found Docker image: ${DOCKER_IMAGE_NAME}:${DOCKER_HUB_IMAGE_VER}"
