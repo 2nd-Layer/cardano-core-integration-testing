751683352a268577f3682dc0e7e15a2940a7995e
Mark Stopka <mark.stopka@perlur.cloud>
2021-03-28 00:37:41 +0100

    ci: Clean-up nightly job (#23)
    * chore: Spin-off OBS functions to separate file
    * Update OBS scripts
    * Enable BuildKit
    * Update Docker Hub check workflow matrix
    * chore: clean-up syntax
    * ci: remove cardano-rt-view from check matrix

--- scripts/obs-core-functions.sh
+++ scripts/obs-core-functions.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+OBS_CONFIG_PROJECTS_JSON_FILE="${GIT_ROOT}/obs/config/projects.json"
+
+OBS_PRJ_STABLE=$(jq -r '.stable' < ${GIT_ROOT}/obs/config/projects.json)
+OBS_PRJ_NIGHTLY=$(jq -r '.nightly' < ${GIT_ROOT}/obs/config/projects.json)
+
+# Enable BuildKit for Docker
+export DOCKER_BUILDKIT=1
+
 function clean_dirty_project() {
   for dir in $(ls); do
     if [ ! -f ${dir}/*.spec ]; then
