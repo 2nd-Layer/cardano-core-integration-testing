86040e01822e294bba1eca370579ef541ea57081
Mark Stopka <mark.stopka@perlur.cloud>
2021-03-28 00:58:36 +0100

    Update nightly job workflow

--- docker/openSUSE/Leap/15.3/plan2rpms.sh
+++ docker/openSUSE/Leap/15.3/plan2rpms.sh
@@ -2,31 +2,45 @@
 
 set -e
 
-PLAN_JSON=$(find -name plan.json)
+export PLAN_ORIG_JSON=$(find $(pwd) -name plan.json)
 
-DEPS_COUNT=$(jq '."install-plan" | length' < ${PLAN_JSON})
+cp ${PLAN_ORIG_JSON} plan.json
 
-for foo in `seq 0 $(($DEPS_COUNT-1))`; do
-  PKG=`jq '."install-plan"['$foo']' < ${PLAN_JSON}`
-  PKG_STYLE=`echo $PKG | jq '.style'`
-  if [ "$PKG_STYLE" = '"global"' ]; then
-    echo $PKG | jq '."pkg-name"' | sed -e 's/^"//' -e 's/"$//' >> hackage-dependencies.txt
-  elif [ "$PKG_STYLE" = '"local"' ]; then
-    echo $PKG | jq '."pkg-name"' | sed -e 's/^"//' -e 's/"$//' >> local-dependencies.txt
-  else
-    true
-  fi
-done
+# Sub-select cabal-install plan to separate file
+jq '."install-plan" | map(.)' < ${PLAN_ORIG_JSON} > plan.install.json
+export PLAN_INSTALL_JSON="$(pwd)/plan.install.json"
+INSTALL_DEPS_COUNT=$(jq '. | length' < ${PLAN_INSTALL_JSON})
 
-sort hackage-dependencies.txt | uniq > hackage-dependencies.txt.tmp
-mv hackage-dependencies.txt.tmp hackage-dependencies.txt
+# Sub-select global (Hackage) dependencies to separate file
+jq '. | map( . | select(.style == "global"))' < ${PLAN_INSTALL_JSON} > plan.global.json
+export PLAN_GLOBAL_JSON="$(pwd)/plan.global.json"
+GLOBAL_DEPS_COUNT=$(jq '. | length' < ${PLAN_GLOBAL_JSON})
 
+# Sub-select local (Git,...) dependencies to separate file
+jq '. | map( . | select(.style == "local"))' < ${PLAN_INSTALL_JSON} > plan.local.json
+export PLAN_LOCAL_JSON="$(pwd)/plan.local.json"
+LOCAL_DEPS_COUNT=$(jq '. | length' < ${PLAN_LOCAL_JSON})
+
+echo "Detected total of ${INSTALL_DEPS_COUNT} dependencies!"
+echo "  Global dependencies: ${GLOBAL_DEPS_COUNT}"
+echo "  Local dependencies: ${LOCAL_DEPS_COUNT}"
+echo "Checksum: $((${GLOBAL_DEPS_COUNT}+${LOCAL_DEPS_COUNT}))"
+
+jq 'map( ."pkg-name") | unique' < ${PLAN_GLOBAL_JSON} | jq -r .[] > hackage-dependencies.txt
+
+jq 'map( ."pkg-name") | unique' < ${PLAN_LOCAL_JSON} | jq -r .[] > local-dependencies.txt
+
+# This loop is quite slow
+echo 'Looking up Hackage dependencies...'
 while read -r line; do
-  if ZYPP_INFO=`zypper search -x ghc-$line-devel`; then
+  if ZYPP_INFO=`zypper search --not-installed-only -x ghc-$line-devel`; then
+    echo "  Found package: ghc-$line-devel"
     echo ghc-$line-devel >> installable-hackage-dependencies.txt
-  elif ZYPP_INFO=`zypper search -x $line`; then
+  elif ZYPP_INFO=`zypper search --not-installed-only -x $line`; then
+    echo "  Found package: $line"
     echo $line >> installable-hackage-dependencies.txt
   else
+    echo "  Did not find package: $line"
     echo $line >> missing-hackage-dependencies.txt
   fi
 done < hackage-dependencies.txt
--- scripts/obs-core-functions.sh
+++ scripts/obs-core-functions.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-if [ ! -z ${GITHUB_WORKSPACE} ]; then
+if [ -z "${GITHUB_WORKSPACE}" ]; then
   GITHUB_WORKSPACE=$(git rev-parse --show-toplevel)
 fi
 
 OBS_CONFIG_PROJECTS_JSON_FILE="${GITHUB_WORKSPACE}/obs/config/projects.json"
 
-OBS_PRJ_STABLE=$(jq -r '.stable' < ${GITHUB_WORKSPACE}/obs/config/projects.json)
-OBS_PRJ_NIGHTLY=$(jq -r '.nightly' < ${GITHUB_WORKSPACE}/obs/config/projects.json)
+OBS_PRJ_STABLE=$(jq -r '.stable' < "${GITHUB_WORKSPACE}/obs/config/projects.json")
+OBS_PRJ_NIGHTLY=$(jq -r '.nightly' < "${GITHUB_WORKSPACE}/obs/config/projects.json")
 
 
 OBS_CONFIG_PROJECTS_JSON_FILE="${GITHUB_WORKSPACE}/obs/config/projects.json"
 
-OBS_PRJ_STABLE=$(jq -r '.stable' < ${GITHUB_WORKSPACE}/obs/config/projects.json)
-OBS_PRJ_NIGHTLY=$(jq -r '.nightly' < ${GITHUB_WORKSPACE}/obs/config/projects.json)
+OBS_PRJ_STABLE=$(jq -r '.stable' < "${GITHUB_WORKSPACE}/obs/config/projects.json")
+OBS_PRJ_NIGHTLY=$(jq -r '.nightly' < "${GITHUB_WORKSPACE}/obs/config/projects.json")
 
 # Enable BuildKit for Docker
 export DOCKER_BUILDKIT=1
